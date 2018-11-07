b3c0ef2c257f021943a801026fdd89820295e811
dubenchuk6@gmail.com <dubenchuk6@gmail.com>
2018-11-07 17:15:54 +0200

    start constructure snake

--- src/classes/snake.js
+++ src/classes/snake.js
@@ -1,19 +1,62 @@
 import SnakePart from "./snake-part";
 
-export default function Snake(length = 0, direction = "right") {
+export default function Snake({direction = 'right', length = 0} = {}) {
+	this.parts = [];
 	this.length = length;
 	this.direction = direction;
-	this.parts = parts;
-	this.head = head;
-	this.length = length;
+	for(let i = 0; i < this.length; i++) {
+		this.parts.push(new SnakePart());
+    this.parts[i]['direction'] = direction;
+  }
+  
+  
+	
+    switch(this.direction) {
+      case "right": 
+      //   this.x +=steps;
+        break;
+      case "left":
+      //   this.x -=steps;
+        break;
+      case "up":
+      //   this.y -=steps;
+        break;
+      case "down":
+      for(let i = 0; i < this.length; i++) {
+          this.parts[i]['y'] = +this.parts[i]['y'] + 1;
+      }
+        break;  
+      }
+	
+	this.head = this.parts[0];
 }
 
-Snake.prototype.parts = new SnakePart();
-
 Snake.prototype.eat = function() {
-
+	this.parts.push(this.parts[this.parts.length-1]);
 };
 
 Snake.prototype.move = function(steps = 1) {
-
+	this.direction = this.head.direction;
+	switch(this.direction) {
+		case "right": 
+		  this.x +=steps;
+		  break;
+		case "left":
+		  this.x -=steps;
+		  break;
+		case "up":
+		  this.y -=steps;
+		  break;
+		case "down":
+		  this.y +=steps;
+		  break;  
+	  }
 };
+
+let config = {
+	direction: 'down',
+	length: 5
+};
+
+let snake = new Snake(config);
+console.log(snake);
