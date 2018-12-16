e5be10a10691b25f84d54c472ac97937e396f8c8
Yulia Dubenchuk <dubenchuk6@gmail.com>
2018-12-16 22:34:20 +0200

    add new function

--- test/spec/base-element.js
+++ test/spec/base-element.js
@@ -1,49 +1,49 @@
 ﻿import BaseElement from '../../src/classes/base-element';
 
 describe('Base Element', function () {
-  describe('interface has a', function () {
-    let element = new BaseElement();
+	describe('interface has a', function () {
+		let element = new BaseElement();
 
-    it('property "x"', function () {
-      expect(element.x).toBeDefined();
-    });
-    it('property "y"', function () {
-      expect(element.y).toBeDefined();
-    });
-    it('property "visible"', function () {
-      expect(element.visible).toBeDefined();
-    });
-  });
+		it('property "x"', function () {
+			expect(element.x).toBeDefined();
+		});
+		it('property "y"', function () {
+			expect(element.y).toBeDefined();
+		});
+		it('property "visible"', function () {
+			expect(element.visible).toBeDefined();
+		});
+	});
 
-  it('property "visible" is `true` by default', function () {
-    let element = new BaseElement();
+	it('property "visible" is `true` by default', function () {
+		let element = new BaseElement();
 
-    expect(element.visible).toBe(true);
-  });
+		expect(element.visible).toBe(true);
+	});
 
-  it('has {x:0, y:0} coordinates by default', function () {
-    let element = new BaseElement();
+	it('has {x:0, y:0} coordinates by default', function () {
+		let element = new BaseElement();
 
-    expect(element.x).toBe(0);
-    expect(element.y).toBe(0);
-  });
+		expect(element.x).toBe(0);
+		expect(element.y).toBe(0);
+	});
 
-  describe('config argument correctly affects a', function () {
-    let config = {
-      x: 774,
-      y: 88,
-      visible: false
-    };
-    let element = new BaseElement(config);
+	describe('config argument correctly affects a', function () {
+		let config = {
+			x: 774,
+			y: 88,
+			visible: false
+		};
+		let element = new BaseElement(config);
 
-    it('property "x"', function () {
-      expect(element.x).toEqual(config.x);
-    });
-    it('property "y"', function () {
-      expect(element.y).toEqual(config.y);
-    });
-    it('property "visible"', function () {
-      expect(element.visible).toEqual(config.visible);
-    });
-  });
+		it('property "x"', function () {
+			expect(element.x).toEqual(config.x);
+		});
+		it('property "y"', function () {
+			expect(element.y).toEqual(config.y);
+		});
+		it('property "visible"', function () {
+			expect(element.visible).toEqual(config.visible);
+		});
+	});
 });
--- test/spec/food.js
+++ test/spec/food.js
@@ -2,71 +2,71 @@
 import BaseElement from '../../src/classes/base-element';
 
 describe('Food', function () {
-  describe('interface has a', function () {
-    let food = new Food();
+	describe('interface has a', function () {
+		let food = new Food();
 
-    it('method "feed()"', function () {
-      expect(food.feed).toBeDefined();
-    });
-  });
+		it('method "feed()"', function () {
+			expect(food.feed).toBeDefined();
+		});
+	});
 
-  describe('recieves a', function () {
-    let food = new Food();
+	describe('recieves a', function () {
+		let food = new Food();
 
-    it('property "x" from a Base Element', function () {
-      expect(food.x).toBeDefined();
-    });
-    it('property "y" from a Base BaseElement', function () {
-      expect(food.y).toBeDefined();
-    });
-    it('property "visible" from a Base Element', function () {
-      expect(food.visible).toBeDefined();
-    });
-  });
+		it('property "x" from a Base Element', function () {
+			expect(food.x).toBeDefined();
+		});
+		it('property "y" from a Base BaseElement', function () {
+			expect(food.y).toBeDefined();
+		});
+		it('property "visible" from a Base Element', function () {
+			expect(food.visible).toBeDefined();
+		});
+	});
 
-  it('is instance of a Base Element', function () {
-    let food = new Food();
+	it('is instance of a Base Element', function () {
+		let food = new Food();
 
-    expect(food instanceof BaseElement).toBe(true);
-  });
+		expect(food instanceof BaseElement).toBe(true);
+	});
 
-  it('property "visible" is `true` by default', function () {
-    let food = new Food();
+	it('property "visible" is `true` by default', function () {
+		let food = new Food();
 
-    expect(food.visible).toBe(true);
-  });
+		expect(food.visible).toBe(true);
+	});
 
-  it('has {x:0, y:0} coordinates by default', function () {
-    let food = new Food();
+	it('has {x:0, y:0} coordinates by default', function () {
+		let food = new Food();
 
-    expect(food.x).toBe(0);
-    expect(food.y).toBe(0);
-  });
+		expect(food.x).toBe(0);
+		expect(food.y).toBe(0);
+	});
 
-  describe('config argument correctly affects a', function () {
-    let config = {
-      x: 774,
-      y: 88,
-      visible: false
-    };
-    let food = new Food(config);
+	describe('config argument correctly affects a', function () {
+		let config = {
+			x: 774,
+			y: 88,
+			visible: false
+		};
+		let food = new Food(config);
 
-    it('property "x"', function () {
-      expect(food.x).toEqual(config.x);
-    });
-    it('property "y"', function () {
-      expect(food.y).toEqual(config.y);
-    });
-    it('property "visible"', function () {
-      expect(food.visible).toEqual(config.visible);
-    });
-  });
+		it('property "x"', function () {
+			expect(food.x).toEqual(config.x);
+		});
+		it('property "y"', function () {
+			expect(food.y).toEqual(config.y);
+		});
+		it('property "visible"', function () {
+			expect(food.visible).toEqual(config.visible);
+		});
+	});
 
-  it('sets "visible" to `false`, when call "feed()"', function () {
-    let food = new Food();
+	it('sets "visible" to `false`, when call "feed()"', function () {
+		let food = new Food();
 
-    food.feed();
-    expect(food.visible).toBe(false);
-  });
+		food.feed();
+		expect(food.visible).toBe(false);
+	});
 
 });
